09430cf86bdd566c756c2c9fbc50bbc6691b0451
hpenney2 <hpenney1010@gmail.com>
2025-05-27 16:44:17 -0500

    handle audio better (will match external pauses) + hopefully help fix preload and prefetch slowness

--- script/player.js
+++ script/player.js
@@ -3,15 +3,24 @@ let audio = new Audio("https://file.garden/aC0AvcHvgSVGyTqS/DVT%20-%20E-ditor%20
 audio.volume = 0.35;
 audio.loop = true;
 
-function toggleAudio(button) {
+function toggleAudio() {
     if (!musicPlaying) {
         audio.play();
-        button.setAttribute("title", "Pause music");
     } else {
         audio.pause();
-        button.setAttribute("title", "Play music");
     }
-
-    musicPlaying = !musicPlaying;
-    button.classList.toggle("musicPlaying");
 }
+
+audio.addEventListener("play", (event) => {
+    const button = document.getElementById("musicButton");
+    musicPlaying = true;
+    button.classList.add("musicPlaying");
+    button.setAttribute("title", "Pause music");
+});
+
+audio.addEventListener("pause", (event) => {
+    const button = document.getElementById("musicButton");
+    musicPlaying = false;
+    button.classList.remove("musicPlaying");
+    button.setAttribute("title", "Play music");
+});
