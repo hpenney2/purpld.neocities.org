91b384fbb3067e005579e8356348897573575e72
hpenney2 <hpenney1010@gmail.com>
2025-06-14 23:56:55 -0500

    WEBKINZ! also an update to the music player to support tooltips and other pages

--- script/player.js
+++ script/player.js
@@ -1,5 +1,29 @@
 let musicPlaying = false;
-let audio = new Audio("https://file.garden/aC0AvcHvgSVGyTqS/DVT%20-%20E-ditor%20kg.mp3");
+let audio;
+let title = "";
+
+switch (window.location.pathname) {
+    case "/shrines/webkinz/":
+        audio = new Audio("https://file.garden/aC0AvcHvgSVGyTqS/Webkinz%20Classic%20OST%20-%20Quizzy%20Question%20Corner%20Theme%20%5BhC-U7QM4DMA%5D.mp3");
+        title = "Webkinz - Quizzy's Question Corner"
+        break;
+
+    case "/shrines/":
+        audio = new Audio("https://file.garden/aC0AvcHvgSVGyTqS/skunksweeper_ost");
+        title = "Webkinz - Skunksweeper Hide N' Skunk"
+        break;
+
+    case "/about/":
+        audio = new Audio("https://file.garden/aC0AvcHvgSVGyTqS/AnimalJam_BestDressed_rec.mp3");
+        title = "Animal Jam - Best Dressed (Preperation)"
+        break;
+
+    default:
+        audio = new Audio("https://file.garden/aC0AvcHvgSVGyTqS/DVT%20-%20E-ditor%20kg.mp3");
+        title = "??? (DVT - E-ditor)"
+        break;
+}
+
 audio.volume = 0.35;
 audio.loop = true;
 
@@ -11,11 +35,19 @@ function toggleAudio() {
     }
 }
 
+let tooltipTimer;
+
 audio.addEventListener("play", (event) => {
     const button = document.getElementById("musicButton");
     musicPlaying = true;
     button.classList.add("musicPlaying");
     button.setAttribute("title", "Pause music");
+
+    document.getElementById("musicTitle").innerHTML = title;
+    button.children[0].classList.add("expanded");
+    tooltipTimer = setTimeout(() => {
+        button.children[0].classList.remove("expanded")
+    }, 10000)
 });
 
 audio.addEventListener("pause", (event) => {
@@ -23,4 +55,7 @@ audio.addEventListener("pause", (event) => {
     musicPlaying = false;
     button.classList.remove("musicPlaying");
     button.setAttribute("title", "Play music");
+    
+    clearTimeout(tooltipTimer);
+    button.children[0].classList.remove("expanded");
 });
