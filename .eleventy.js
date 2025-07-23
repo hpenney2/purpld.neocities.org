8c557bf3fe3e370a6a6ee9b4307d69d2b488c89b
hpenney2 <hpenney1010@gmail.com>
2025-07-23 02:18:01 -0500

    use templates for defrag zone posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require("luxon");
+
 module.exports = function (eleventyConfig) {
 
   // This will stop the default behaviour of foo.html being turned into foo/index.html
@@ -23,6 +25,30 @@ module.exports = function (eleventyConfig) {
 //     failOnError: false,
 //   });
 
+  // https://stackoverflow.com/a/78200088
+  eleventyConfig.addFilter("date", function (value, format = "yyyy-LL-dd") {
+    if (!value) return "";
+
+    if (value instanceof Date) {
+      return DateTime.fromJSDate(value, {
+        zone: 'utc',
+        locale: "en"
+      }).toFormat(format);
+    }
+    else {
+      return DateTime.fromISO(value, {
+        zone: 'utc',
+        locale: "en"
+      }).toFormat(format);
+    }
+  });
+  
+  // https://caffeineandlasers.neocities.org/blogs/automatingYourRSS
+  eleventyConfig.addCollection("defragPosts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("content/defrag/entries/*.{html,md}").sort((a, b) => {
+      return b.date - a.date; // Sort in reverse chronological order (newest first)
+    });
+  });
 
   return {
     dir: {
